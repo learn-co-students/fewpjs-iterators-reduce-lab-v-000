6db30615565340472d58ba0802eaca6ea9eae99e
github username <github email address>
2019-10-28 09:56:59 -0400

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,22 @@
 const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
 
+const totalBatteries = batteryBatches.reduce( (total, batch) => total + batch, 0)
+
+
+
+
+
 // Code your solution here
+// work with lists and transform elements vs
+// aggregating, reduce the list to a single value 
+// products.forEach(product => {
+	// get something useful out of each element and 
+	// make it a final value or summary object 
+
+//reduce the object coupon locations by the functin couponCounter 
+// 3d arg is starting point 
+
+// THIS IS THE FORMULA 
+// const totalbatteries = batteryBatches.reduce( (total, element) =>{
+// 	total + element, 0
+// })
