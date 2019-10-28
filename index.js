ac9b93365844e4f9b05f4af43e0c627bfa5709e5
github username <github email address>
2019-10-28 09:58:52 -0400

    Done.

--- index.js
+++ index.js
@@ -1,9 +1,6 @@
 const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
 
-const totalBatteries = batteryBatches.reduce( (total, batch) => {
-	const tB = total + batch, 0
-	return tB
-	})
+const totalBatteries = batteryBatches.reduce( (total, batch) => total + batch, 0)
 
 
 
