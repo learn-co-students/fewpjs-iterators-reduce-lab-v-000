6c301cc7a93be2338b498b0b618515f3caaa7c0a
lizbur10 <liz.burton147@gmail.com>
2020-01-29 15:24:18 +0300

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,24 @@
 const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
 
 // Code your solution here
+
+const totalBatteries = batteryBatches.reduce((total, batch) => total + batch, 0);
+
+
+
+
+
+
+
+
+
+
+// FIX CODE FOR REDUCE LAB
+// function ourReduce(couponLocations, couponCounter, init) {
+//     let totalAmount = init;
+//     couponLocations.forEach(coupon => {
+//         totalAmount = couponCounter(totalAmount, coupon) });
+//     return totalAmount;
+// }
+
+// Also typo: `If no initial value is supplies, t`
