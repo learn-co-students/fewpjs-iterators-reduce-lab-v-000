3817f33fb2239cce6f082abc3483c1c20177cab3
github username <github email address>
2020-05-10 18:12:27 -0400

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,17 @@
 const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
 
 // Code your solution here
+//Create a new variable called totalBatteries which is the sum of all of the
+//battery amounts in the batteryBatches array. Naturally, use reduce() for this!
+
+
+const reducer = (accumulator, currentValue) => accumulator + currentValue
+
+const totalBatteries = batteryBatches.reduce(reducer)
+
+//console.log(couponLocations.reduce(couponCounter, 0)); // also prints 15!
+
+//incoming_students.reduce( (houses, student) => houses[sorting_hat.assign(student)].push(student) , hogwarts_houses)
+
+
+//console.log(couponLocations.reduce(couponCounter, 0)); // also prints 15!
